5cb10e9e816430fb392742274fe1b7f5472fcf97
shuqidiudiu <vistasfh@qq.com>
2025-06-15 21:20:58 +0800

    update first program

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // This is a simple C++ program that prints "Hello, World!" to the console.
+    // It uses the iostream library for input and output operations.
+    getchar();
+    return 1;
+}
